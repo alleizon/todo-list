9ad982b6f2ac4c62e9d36463c41943fb23d90cdd
alleizon <112495304+alleizon@users.noreply.github.com>
2023-01-28 20:30:33 +0200

    initial config

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const lol = document.createElement("div");
+lol.style.height = "200px";
+lol.style.backgroundColor = "red";
+document.querySelector("body").appendChild(lol);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGxvbCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoXCJkaXZcIik7XG5sb2wuc3R5bGUuaGVpZ2h0ID0gXCIyMDBweFwiO1xubG9sLnN0eWxlLmJhY2tncm91bmRDb2xvciA9IFwicmVkXCI7XG5kb2N1bWVudC5xdWVyeVNlbGVjdG9yKFwiYm9keVwiKS5hcHBlbmRDaGlsZChsb2wpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
